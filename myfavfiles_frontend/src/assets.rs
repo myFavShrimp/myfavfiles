2269b02a261d745556b27c2e4dd81d11b7cd9a0d
myFavShrimp <myfavshrimp@tuta.io>
2022-08-19 08:43:59 +0200

    Added embedded frontend

---
+++ myfavfiles_frontend/src/assets.rs
@@ -0,0 +1,8 @@
+#[iftree::include_file_tree("
+    paths = '**'
+    base_folder = '../frontend'
+")]
+pub struct Asset {
+    pub relative_path: &'static str,
+    pub contents_str: &'static str,
+}   
--- myfavfiles_frontend/src/lib.rs
+++ myfavfiles_frontend/src/lib.rs
@@ -1,9 +1,11 @@
 use std::path::PathBuf;
 
-use axum::{body::Body, http::Request, response::IntoResponse, routing::get_service, Router};
+use axum::{body::Body, http::{Request, StatusCode}, response::{IntoResponse, Html}, routing::{get_service, get}, Router};
 use myfavfiles_common as common;
 use tower::ServiceExt;
 use tower_http::services::{ServeDir, ServeFile};
+
+mod assets;
 
 pub fn create_frontend_router() -> Router {
     let frontend_path = PathBuf::from(common::config::Config::default().frontend_path);
@@ -12,9 +14,20 @@ pub fn create_frontend_router() -> Router {
 
     Router::new().nest(
         "/",
-        get_service(ServeDir::new(frontend_path).fallback(ServeFile::new(index_file_path)))
-            .handle_error(common::handler::error_handler_500),
+        get(serve)
     )
+}
+
+async fn serve(req: Request<Body>) -> impl IntoResponse {
+    let path = req.uri().path().trim_start_matches("/");
+
+    dbg!(path);
+    assets::ASSETS.iter().for_each(|a| {dbg!(a.relative_path);});
+
+    match assets::ASSETS.iter().position(|asset| asset.relative_path == path) {
+        None => Html::from(assets::base::INDEX_HTML.contents_str.to_owned()),
+        Some(index) => Html::from(assets::ASSETS[index].contents_str.to_owned()),
+    }
 }
 
 pub async fn fallback_frontend_handler(req: Request<Body>) -> impl IntoResponse {
