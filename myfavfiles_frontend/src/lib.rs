d937e029291cc9cd52ea4c8f62dc246aca120ac4
myFavShrimp <myfavshrimp@tuta.io>
2022-08-19 18:35:45 +0200

    Added conditional imports

--- myfavfiles_frontend/src/lib.rs
+++ myfavfiles_frontend/src/lib.rs
@@ -1,10 +1,17 @@
 use std::path::PathBuf;
 
-use axum::{body::Body, http::{Request, StatusCode}, response::{IntoResponse, Html}, routing::{get_service, get}, Router};
+use axum::{body::Body, http::Request, response::{IntoResponse}, Router};
 use myfavfiles_common as common;
 use tower::ServiceExt;
-use tower_http::services::{ServeDir, ServeFile};
 
+#[cfg(debug_assertions)]
+use tower_http::services::{ServeDir, ServeFile};
+#[cfg(debug_assertions)]
+use axum::routing::get_service;
+
+#[cfg(not(debug_assertions))]
+use axum::{response::Html, routing::get};
+#[cfg(not(debug_assertions))]
 mod assets;
 
 pub fn create_frontend_router() -> Router {
@@ -14,14 +21,20 @@ pub fn create_frontend_router() -> Router {
 
     Router::new().nest(
         "/",
+
+        #[cfg(debug_assertions)]
+        get_service(ServeDir::new(frontend_path).fallback(ServeFile::new(index_file_path)))
+            .handle_error(common::handler::error_handler_500),
+
+        #[cfg(not(debug_assertions))]
         get(serve)
     )
 }
 
+#[cfg(not(debug_assertions))]
 async fn serve(req: Request<Body>) -> impl IntoResponse {
     let path = req.uri().path().trim_start_matches("/");
 
-    dbg!(path);
     assets::ASSETS.iter().for_each(|a| {dbg!(a.relative_path);});
 
     match assets::ASSETS.iter().position(|asset| asset.relative_path == path) {
