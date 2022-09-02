f49c26f73b7349e479248635667c4a1ab26a9979
myFavShrimp <myfavshrimp@tuta.io>
2022-09-02 08:07:19 +0200

    Added test for password hash renewal

--- myfavfiles_api/src/database/password.rs
+++ myfavfiles_api/src/database/password.rs
@@ -15,3 +15,19 @@ pub fn new_password_hash_maybe(
         Ok(None)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::new_password_hash_maybe;
+
+    #[test]
+    fn generate_new_hashwith_better_cost() {
+        let password = "Xtrem Lee Secure";
+        let old_hash = bcrypt::hash(password, 7).unwrap();
+
+        let new_hash_maybe = new_password_hash_maybe(password, &old_hash, 14).unwrap();
+
+        assert!(new_hash_maybe.is_some());
+        assert!(new_hash_maybe.unwrap() != old_hash);
+    }
+}
